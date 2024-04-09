d4974b4e5da0ce2345181ed4edf9517474a3f483
Rowlytech <74044172+Rowlytech@users.noreply.github.com>
2024-04-09 18:54:59 +0100

    Add files via upload

---
+++ scripts/signup.js
@@ -0,0 +1,4 @@
+const name = document.querySelector('.name');
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const button = document.querySelector('button');
